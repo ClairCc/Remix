245f18f62bc173f9802c090ea7e52f0c180b1a98
clair <cavanzoariasc@gmail.com>
2021-09-25 06:14:15 -0500

    Realizando el menu para dispositivos mobiles

--- js/header.js
+++ js/header.js
@@ -1,8 +1,14 @@
 const menu = document.getElementById("menu");
 const header = document.getElementById("header_nav");
+const closeMenu = document.getElementById("nav_close-container");
 
 menu.addEventListener("click", function () {
   menu.classList.add("header_figure-menu-off");
   header.classList.add("header_nav-on");
   header.classList.remove("header_nav");
+});
+closeMenu.addEventListener("click", function () {
+  header.classList.add("header_nav");
+  header.classList.remove("header_nav-on");
+  menu.classList.remove("header_figure-menu-off");
 });
