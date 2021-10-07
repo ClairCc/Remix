7f94dc609aa11f88aa4ea56c02c2255d32afebf5
clair <cavanzoariasc@gmail.com>
2021-10-07 08:55:00 -0500

    agregando logica de pages interan en el login

--- js/login.js
+++ js/login.js
@@ -11,6 +11,19 @@ let blackColors = [
   "#434450",
   "#DFE0E2",
 ];
+const back = document.getElementsByClassName("back");
+const next = document.getElementsByClassName("next");
+const hero = document.getElementsByClassName("hero");
+
+const foundDisplay = (object) => {
+  let mostrar = window.getComputedStyle(object);
+  let heroDisplay = mostrar.getPropertyValue("display");
+  return heroDisplay;
+};
+let next1 = document.getElementById("next1");
+let next2 = document.getElementById("next2");
+let anterior1 = document.getElementById("anterior");
+let anterior2 = document.getElementById("anterior2");
 
 const root = document.documentElement;
 const navContainer = document.getElementById("header_nav-icons");
@@ -38,3 +51,43 @@ const changeColorsToWhite = () => {
 
 ligthMode.addEventListener("click", changeColorsToWhite);
 moon.addEventListener("click", changeColorsToBlack);
+let contador = 0;
+const suma = (i) => {
+  const siguiente = () => {
+    if (foundDisplay(hero[i]) === "flex" && contador < 2) {
+      hero[i].style.display = "none";
+    }
+    ++contador;
+    if (contador < 3) {
+      hero[contador].style.display = "flex";
+    }
+  };
+  return siguiente();
+};
+const resta = (i) => {
+  const siguiente = () => {
+    if (
+      foundDisplay(hero[i]) === foundDisplay(hero[1]) ||
+      foundDisplay(hero[i]) === foundDisplay(hero[2])
+    ) {
+      hero[i].style.display = "none";
+      hero[(i -= 1)].style.display = "flex";
+      contador -= 1;
+    }
+  };
+  return siguiente();
+};
+
+let x = suma;
+let y = resta;
+
+function ejecutar() {
+  x(contador);
+}
+function restar() {
+  y(contador);
+}
+next1.addEventListener("click", ejecutar);
+next2.addEventListener("click", ejecutar);
+anterior1.addEventListener("click", restar);
+anterior2.addEventListener("click", restar);
